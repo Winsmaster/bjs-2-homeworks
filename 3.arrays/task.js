86c47e70687ebb325d316342d50642ce09d39828
Winsmaster <amelmurm@yandex.ru>
2022-06-23 18:31:24 +0300

    3 задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,6 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
   // Ваш код
 
   return result; // boolean
@@ -8,8 +8,7 @@ function compareArrays(arr1, arr2) {
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item*10); 
+ 
   return resultArr; // array
 }
