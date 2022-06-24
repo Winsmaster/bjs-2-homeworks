1f1c5c8c0e01d5905571e4af1c4a025958718f2c
Winsmaster <amelmurm@yandex.ru>
2022-06-24 19:21:48 +0300

    Задача 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){ 
+      this.marks = [mark];
+           } else {
+        this.marks.push(mark);        
+    }
+ }
+
+ Student.prototype.addMarks = function (...mark) {
+      this.marks = [].slice.call(mark);
+}
+
+  Student.prototype.getAverage = function (getAverage) {
+      const sum = this.marks.reduce((acc, number) => acc + number, 0);
+      const length = this.marks.length;
+      getAverage = sum / length;
+      return getAverage;
+  }
+   
+   Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;  
+ }
